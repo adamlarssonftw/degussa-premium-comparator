69312ce61f1373647bd4f4c1a2b784e519e41164
Adam <adamlarsson92@gmail.com>
2017-05-11 13:16:05 +0200

    Split main blob into separate functions.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ class Main {
     private platinumItems: IItem[];
     private spotPrices: any;
 
-    // private spotPricesURL = "https://www.gold.de/kurse/";
     private spotPricesURL = "https://www.gold.de/ajax/data.php?";
     private priceDataSourceURL = "http://www.degussa-goldhandel.de/infothek/preisliste/";
     private propertiesOfInterest = ["au_gold_eur", "au_silber_eur", "au_platin_eur", "au_palladium_eur"];
@@ -20,21 +19,24 @@ class Main {
 
     constructor() {
         this.scraper = new Scraper();
+        this.getMetalShopPrices();
+        this.getMetalSpotPrices();
+    }
 
+    public getMetalShopPrices() {
         this.scraper.attemptRequest(this.priceDataSourceURL).subscribe((html: string) => {
             this.scraper.loadCheerioWithHtml(html);
             this.goldItems = this.scraper.getItemsForSale("#tab2");
-            //    this.silverItems = this.scraper.getItemsForSale("#tab3");
-            //    this.platinumItems = this.scraper.getItemsForSale("#tab4");
-            //    console.log(Calculator.sortBySpread(this.goldItems).slice(0, 10));
+            this.silverItems = this.scraper.getItemsForSale("#tab3");
+            this.platinumItems = this.scraper.getItemsForSale("#tab4");
         });
+    }
 
+    public getMetalSpotPrices() {
         let timestampedURL = `${this.spotPricesURL}func=json&_=${Utils.generateTimestamp()}`;
-
         this.scraper.attemptRequest(timestampedURL).subscribe((response: string) => {
             let responseObject = JSON.parse(response);
             this.spotPrices = this.scraper.getSpotPrices(responseObject, this.propertiesOfInterest);
-            console.log(this.spotPrices);
         });
     }
 }
